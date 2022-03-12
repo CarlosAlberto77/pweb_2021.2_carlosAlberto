eab3fc6482af9240628214ac47e4dcf9e499901e
CarlosAlberto77 <parrera.ca@gmail.com>
2022-03-11 21:47:54 -0300

    Criação do Exercício 3

---
+++ u1_exercicio3/cadpessoas_carlosalberto/src/main/java/br/com/carlosalberto/cadpessoas/CadpessoasCarlosalbertoApplication.java
@@ -0,0 +1,13 @@
+package br.com.carlosalberto.cadpessoas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadpessoasCarlosalbertoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadpessoasCarlosalbertoApplication.class, args);
+	}
+
+}
---
+++ u1_exercicio3/cadpessoas_carlosalberto/src/test/java/br/com/carlosalberto/cadpessoas/CadpessoasCarlosalbertoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.carlosalberto.cadpessoas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadpessoasCarlosalbertoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
