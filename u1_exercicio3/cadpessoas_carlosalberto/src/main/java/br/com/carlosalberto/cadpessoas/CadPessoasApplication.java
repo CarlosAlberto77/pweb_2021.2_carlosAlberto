f7cd1c2559d86decc0e648d155ef5779d151248a
CarlosAlberto77 <parrera.ca@gmail.com>
2022-03-24 10:14:42 -0300

    Parte - Exercício 3

--- u1_exercicio3/cadpessoas_carlosalberto/src/main/java/br/com/carlosalberto/cadpessoas/CadPessoasApplication.java
+++ u1_exercicio3/cadpessoas_carlosalberto/src/main/java/br/com/carlosalberto/cadpessoas/CadPessoasApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CadpessoasCarlosalbertoApplication {
+public class CadPessoasApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CadpessoasCarlosalbertoApplication.class, args);
+		SpringApplication.run(CadPessoasApplication.class, args);
 	}
 
 }
---
+++ u1_exercicio3/cadpessoas_carlosalberto/src/main/java/br/com/carlosalberto/cadpessoas/model/Pessoa.java
@@ -0,0 +1,73 @@
+package br.com.carlosalberto.cadpessoas.model;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+
+import javax.annotation.Generated;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Table;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name="pessoas")
+
+public class Pessoa implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+    private String email;
+    private String genero;
+
+    @Column(nullable = false)
+    @DateTimeFormat(pattern="yyyy-MM-dd")
+    private LocalDate dataNascimento;
+
+    public Long getId(){
+        return id;
+    }
+    public void setId(Long id){
+        this.id = id;
+    }
+
+    public String getNome(){
+        return nome;
+    }
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+
+    public String getEmail(){
+        return email;
+    }
+    public void setEmail(String email){
+        this.email = email;
+    }
+
+    public String getGenero(){
+        return genero;
+    }
+    public void setGenero(String genero){
+        this.genero = genero;
+    }
+
+    public LocalDate getDataNascimento(){
+        return dataNascimento;
+    }
+    public void setDataNascimento(LocalDate dataNascimento){
+        this.dataNascimento = dataNascimento;
+    }
+}
