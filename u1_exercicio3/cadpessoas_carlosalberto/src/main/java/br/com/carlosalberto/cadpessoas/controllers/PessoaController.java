bca2f41b6d56e2d481fbc297018b6a75bf70379d
CarlosAlberto77 <parrera.ca@gmail.com>
2022-03-31 10:23:51 -0300

    D do CRUD editado

--- u1_exercicio3/cadpessoas_carlosalberto/src/main/java/br/com/carlosalberto/cadpessoas/controllers/PessoaController.java
+++ u1_exercicio3/cadpessoas_carlosalberto/src/main/java/br/com/carlosalberto/cadpessoas/controllers/PessoaController.java
@@ -18,6 +18,10 @@ public class PessoaController{
     @Autowired
     PessoaRepository pessoaRepo;
 
+    PessoaController(PessoaRepository pessoaRepo){
+        this.pessoaRepo = pessoaRepo;
+    }
+
     @GetMapping
     public String index() {
         return "index.html";
@@ -26,8 +30,19 @@ public class PessoaController{
     @GetMapping("/listarPessoas")
     public ModelAndView listarPessoas(){
         List<Pessoa> lista = pessoaRepo.findAll();
+
         ModelAndView mav = new ModelAndView("listaPessoas");
+
         mav.addObject("pessoas", lista);
+
         return mav;
+    }
+    @GetMapping("/remover/{id}")
+    public ModelAndView removerPessoa(@PathVariable("id") long id){
+        Pessoa aRemover = pessoaRepo.findById(id)
+        .orElseThrow(() -> new IllegalArgumentException("ID inválido:" + id));
+
+        pessoaRepo.delete(aRemover);
+        return new ModelAndView("redirect:/listarPessoas");
     }
 }
